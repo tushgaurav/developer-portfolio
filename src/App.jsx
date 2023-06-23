20000a462226c02bc39c651b1091d9c412c36164
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-06-23 18:57:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="app">
+        <div>
+          <h1>Website Under Construction</h1>
+          <h2>aniket.us</h2>
+        </div>
+      </div>
+    </>
+  );
+}
