20aa5c2f129daeb3d112d3ec378010dae8ad5b6d
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-07-09 08:50:46 +0530

    updated social links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,19 +1,28 @@
-import "./Social.css"
+import "./Social.css";
 
-const Social = function() {
-        return (
-            <div className="social">
-                <a href="">
-                    <i className="fab fa-github"></i>
-                </a>
-                <a href="">
-                    <i className="fab fa-linkedin"></i>
-                </a>
-                <a href="">
-                    <i className="fab fa-twitter"></i>
-                </a>
-            </div>
-        )
-}
+const Social = function () {
+  return (
+    <div className="social">
+      <a href="https://github.com/aniketaman17">
+        <i className="fab fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/aniket-aman/">
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <a href="https://twitter.com/aniketaman177">
+        <i className="fab fa-twitter"></i>
+      </a>
+      <a href="https://dev.to/aniketaman17">
+        <i className="fab fa-dev"></i>
+      </a>
+      <a href="mailto:aniketaman18@gmail.com">
+        <i className="fas fa-envelope"></i>
+      </a>
+      <a href="https://mastodon.social/@aniketaman17">
+        <i className="fab fa-mastodon"></i>
+      </a>
+    </div>
+  );
+};
 
 export default Social;
